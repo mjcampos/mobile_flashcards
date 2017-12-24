61550e9b28a906eefc4fec849270508103471d86
Michael Campos <camposmj89@gmail.com>
2017-12-23 18:18:27 -0800

    Initial commit

---
+++ src/DeckList.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+class DeckList extends Component {
+	render() {
+		return (
+			<View>
+				<Text>DeckList.js</Text>
+			</View>
+		);
+	}
+}
+
+export default DeckList;
---
+++ src/NewDeck.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+class NewDeck extends Component {
+	render() {
+		return (
+			<View>
+				<Text>NewDeck.js</Text>
+			</View>
+		);
+	}
+}
+
+export default NewDeck;
