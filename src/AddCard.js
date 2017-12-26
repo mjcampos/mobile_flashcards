91e742a347a02929e18bab38c313f4c0d6317c6e
Michael Campos <camposmj89@gmail.com>
2017-12-25 21:47:17 -0800

    Build out AddCard component. I'm at the point that we just need to wire up the submission to a helper.js method

--- src/AddCard.js
+++ src/AddCard.js
@@ -1,14 +1,72 @@
 import React, {Component} from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button, Keyboard } from 'react-native';
 
 class AddCard extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			question: '',
+			answer: ''
+		}
+	}
+
+	onUserSubmit = () => {
+		var {question, answer} = this.state;
+
+		if(question.trim().length && answer.trim().length) {
+			console.log("Valid");
+		} else {
+			console.log("Invalid");
+		}
+		
+		this.setState({
+			question: '',
+			answer: ''
+		});
+
+		Keyboard.dismiss();
+	}
+
 	render() {
+		const {viewStyles, textInputStyles} = styles;
+		var {question, answer} = this.state;
+
 		return (
-			<View>
-				<Text>AddCard.js</Text>
+			<View style={viewStyles}>
+				<TextInput
+					style={textInputStyles}
+					autoCapitalize={'sentences'}
+					placeholder={'Insert Question'}
+					value={question}
+					onChangeText={question => this.setState({question})}
+				/>
+
+				<TextInput
+					style={textInputStyles}
+					autoCapitalize={'sentences'}
+					placeholder={'Insert Answer'}
+					value={answer}
+					onChangeText={answer => this.setState({answer})}
+				/>
+
+				<Button title="Submit" onPress={this.onUserSubmit}/>
 			</View>
 		);
 	}
 }
+
+const styles = StyleSheet.create({
+	viewStyles: {
+		flex: 1,
+		padding: 20
+	},
+	textInputStyles: {
+		alignItems: 'stretch',
+		height: 40,
+		borderWidth: 2,
+		alignItems: 'stretch',
+		marginTop: 20
+	}
+});
 
 export default AddCard;
