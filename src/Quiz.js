9c79be17d7576bf7f1f96c76662247907e70d54f
Michael Campos <camposmj89@gmail.com>
2017-12-26 13:12:33 -0800

    Built out Quiz.js for users to get tested on their deck card knowledge

--- src/Quiz.js
+++ src/Quiz.js
@@ -1,21 +1,100 @@
 import React, {Component} from 'react';
+import { NavigationActions } from 'react-navigation'
 import { StyleSheet, Text, View, Button } from 'react-native';
 import {connect} from 'react-redux';
 
 class Quiz extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			index: 0,
+			correct: 0,
+			revealAnswer: false
+		}
+	}
+
+	correctAnswer = () => {
+		var {index, correct} = this.state;
+
+		this.setState({
+			index: index + 1,
+			correct: correct + 1
+		});
+	}
+
+	incorrectAnswer = () => {
+		var {index} = this.state;
+
+		this.setState({
+			index: index + 1
+		});
+	}
+
 	render() {
+		const {viewStyles, questionStyles} = styles;
+		var {index, correct, revealAnswer} = this.state;
+		var {questions} = this.props;
+
+		var showQuestion = () => {
+			return (
+				<View style={questionStyles}>
+					<Text>{questions[index].question}</Text>
+					<Button
+						title="Answer" onPress={() => this.setState({revealAnswer: !this.state.revealAnswer})}
+					/>
+					<Button title="Correct" onPress={() => this.correctAnswer()}/>
+					<Button title="Incorrect" onPress={() => this.setState({index: index + 1})}/>
+				</View>
+			);
+		}
+
+		var showAnswer = () => {
+			return (
+				<View style={questionStyles}>
+					<Text>{questions[index].answer}</Text>
+					<Button
+						title="Question" onPress={() => this.setState({revealAnswer: !this.state.revealAnswer})}
+					/>
+				</View>
+			);
+		}
+
 		return (
 			<View>
-				<Text>Quiz.js</Text>
+				<View>
+					<Text>{correct} / {questions.length}</Text>
+				</View>
+
+				{index >= questions.length ? (
+					<View style={questionStyles}>
+						<Text>Quiz is complete</Text>
+						<Button
+							title="Back"
+							onPress={() => this.props.navigation.dispatch(NavigationActions.back())}
+						/>
+					</View>
+				) : revealAnswer ? showAnswer() : showQuestion()}
 			</View>
 		);
+
 	}
 }
+
+const styles = StyleSheet.create({
+	viewStyles: {
+		flex: 1
+	},
+	questionStyles: {
+		alignItems: 'center'
+	}
+});
 
 function mapStateToProps(state, props) {
 	var {title} = props.navigation.state.params;
 
-	return state;
+	return {
+		questions: state.decks[title].questions
+	};
 }
 
 export default connect(mapStateToProps, null)(Quiz);
